843abab76a481ea93d30d5133ad5afc77801e4c8
AnthoHansen <62178203+AnthoHansen@users.noreply.github.com>
2025-01-30 16:49:19 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+version '1.0.0'
+author 'AnthoHansen'
+description 'Get all commands of your server and send them to a fancy discord webhook, make your admin job easier'
+
+server_script 'allcommands.lua'
+shared_script 'config.lua'
