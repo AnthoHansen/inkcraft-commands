0e5e614523f1128f7874aed3483f778a016c6aa9
AnthoHansen <62178203+AnthoHansen@users.noreply.github.com>
2025-01-30 16:49:33 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config.WebhookUrl = 'YOUR_WEBHOOK_URL'
+
+Config.Command = 'commandlist'
+
+Config.BlacklistCommands = {
+    "+",
+    "-",
+    "_",
+    "sv_",
+    "adhesive_",
+    "citizen_",
+    "con_",
+    "endpoint_",
+    "fileserver",
+    "load_server",
+    "mysql_connection",
+    "net_tcp",
+    "netPort",
+    "netlib",
+    "onesync",
+    "onesync_",
+    "rateLimiter_",
+    "svgui",
+    "web_base",
+    "temp_",
+}
